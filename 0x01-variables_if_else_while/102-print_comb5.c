6454f117f0254c7eb73c189eb4c96722038f8274
TheGreatestbug <izyspikes@gmail.com>
2023-05-23 10:35:08 +0100

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,13 +18,13 @@ int main(void)
 	{
 		for (k = i + 1; k <= 99; k++)
 		{
-			putchar(i /10 + '0');
+			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
 			putchar(' ');
 			putchar(k / 10 + '0');
 			putchar(k % 10 + '0');
 
-			if(i != 98 || k != 99)
+			if (i != 98 || k != 99)
 			{
 				putchar(',');
 				putchar(' ');
