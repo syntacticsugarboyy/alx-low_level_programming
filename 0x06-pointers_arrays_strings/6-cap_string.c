5d701d6c561330a8b142b208370e23887b157715
TheGreatestbug <izyspikes@gmail.com>
2023-06-03 00:43:51 +0100

    Capitalises selected letters

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes strings
+ * @b: String to be capitalized
+ *
+ * Description: A function that capitalizes words in a string
+ *
+ * Return: pointer to the string
+ */
+
+char *cap_string(char *b)
+{
+	int index;
+	int next;
+
+	for (index = 0; b[index] != '\0'; index++)
+	{
+		if (b[index] == ' ' ||
+				b[index] == '\t' ||
+				b[index] == '\n' ||
+				b[index] == '!' ||
+				b[index] == ',' ||
+				b[index] == ';' ||
+				b[index] == '.' ||
+				b[index] == '"' ||
+				b[index] == '?' ||
+				b[index] == '(' ||
+				b[index] == ')' ||
+				b[index] == '{' ||
+				b[index] == '}')
+		{
+			next = 1;
+		}
+
+		else if (next && (b[index] >= 97 && b[index] <= 122))
+		{
+			b[index] = b[index] - 32;
+			next = 0;
+		}
+
+		else
+		{
+			next = 0;
+		}
+	}
+
+	return (b);
+}
