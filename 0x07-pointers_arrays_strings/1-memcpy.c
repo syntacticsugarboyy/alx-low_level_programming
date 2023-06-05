f837cb5d4a8d26018ec5030f0adcd14d3122c055
TheGreatestbug <izyspikes@gmail.com>
2023-06-05 12:10:21 +0100

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,7 +15,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 
-	for(index = 0; index <= n && src[index] != '\0'; index++)
+	for (index = 0; index <= n && src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
 	}
