8ad9db4e4e4ec2e9f079d6d167f851db0a1b4a45
TheGreatestbug <izyspikes@gmail.com>
2023-06-07 00:44:33 +0100

    Advanced++

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints sum of diagonals
+ * @a: pointer to the array
+ * @size: number of diagnoals to be summed
+ *
+ * Description: A function that prints the sum of diagonals in a matrix
+ *
+ * Return: sum
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int x;
+	int y;
+	int sum;
+
+	for (x = 0; x)
+}
