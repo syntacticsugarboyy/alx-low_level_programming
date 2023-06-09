09a46264a28d5350ad95450450cdc12d53a4150c
TheGreatestbug <izyspikes@gmail.com>
2023-06-10 00:58:36 +0100

    Prints argv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc - argument count
+ * @argv - argument vector
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
