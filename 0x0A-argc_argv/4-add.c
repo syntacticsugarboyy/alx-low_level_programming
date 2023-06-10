0e12504f9e660a350cb85bb4a1b317c13eb31ea5
TheGreatestbug <izyspikes@gmail.com>
2023-06-10 02:39:11 +0100

    Adds positive arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,26 +14,50 @@ int main(int argc, char **argv)
 {
 	int index;
 	int num;
-	int ll;
-
 	int sum = 0;
 
 	for (index = 1; index < argc; index++)
 	{
-		for (ll = 0; argv[index][ll] != '\0'; ll++)
-		{
-			if (!isdigit(argv[index][ll]))
-			{
-				printf("Error\n");
+		num = is_num(argv[index]);
 
-				return (1);
-			}
+		if (num == -1)
+		{
+			printf("Error\n");
+
+			return (1);
 		}
-		num += atoi(argv[index]);
-		sum += num;
+		sum += atoi(argv[index]);
 	}
 
 	printf("%d\n", sum);
 
 	return (0);
 }
+
+/**
+ * is_num - checks if a number or not
+ * @c: char to be checked
+ * Return: 0 or -1
+ */
+
+int is_num(char *c)
+{
+	int i, num, len;
+
+	i = 0;
+	num = 0;
+	len = strlen(c);
+
+	while (i < len)
+	{
+		if (c[i] < '0' || c[i] > '9')
+		{
+			return (-1);
+		}
+		else
+			num = num * 10 + (c[i] - '0');
+		i++;
+	}
+	return (num);
+}
+
