d774414db26ef1fe0144d28ef31d4e84f3c55511
TheGreatestbug <izyspikes@gmail.com>
2023-06-10 03:05:18 +0100

    Calculates change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,68 +1,45 @@
 #include "main.h"
-
 /**
  * main - Entry point
  * @argc: argument count
  * @argv: argument vector
- *
  * Description: A program that prints change
- *
  * Return: 0 or 1
  */
-
 int main(int argc, char *argv[])
 {
-	int amount;
-	int coins;
+	int amount, coins;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-
 		return (1);
 	}
-
 	amount = atoi(argv[1]);
 	coins = 0;
 
 	if (amount > 25)
 	{
 		while (amount >= 25)
-		{
-			amount -= 25;
-			coins++;
-		}
+			amount -= 25, coins++;
 	}
 	if (amount > 10 && amount < 25)
 	{
 		while (amount >= 10)
-		{
-			amount -= 10;
-			coins++;
-		}
+			amount -= 10, coins++;
 	}
 	if (amount > 5 && amount < 10)
 	{
 		while (amount >= 5)
-		{
-			amount -= 5;
-			coins++;
-		}
+			amount -= 5, coins++;
 	}
 	if (amount > 2 && amount < 5)
 	{
 		while (amount >= 2)
-		{
-			amount -= 5;
-			coins++;
-		}
+			amount -= 5, coins++;
 	}
-
 	if (amount == 1 || amount == 2 || amount == 5 || amount == 10 || amount == 25)
-	{
 		coins++;
-	}
 	printf("%d\n", coins);
-
 	return (0);
 }
