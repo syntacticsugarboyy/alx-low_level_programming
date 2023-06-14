da01ac5558522aa11accc781e1becf2ca3841ea5
TheGreatestbug <izyspikes@gmail.com>
2023-06-14 01:57:23 +0100

    Converts arguments to strings

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * argstostr - args to string
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Description: A function that converts arguments to a string
+ *
+ * Return: args
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *args;
+	int i, n;
+	int r = 0;
+	int l = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			l++;
+		l++;
+	}
+
+	args = malloc(sizeof(char) * l);
+
+	if (args == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			args[r] = av[i][n];
+			r++;
+		}
+		if (args[r] == '\0')
+		{
+			args[r++] = '\n';
+		}
+	}
+
+	return (args);
+}
