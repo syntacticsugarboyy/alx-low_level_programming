29f515ec604cb4dfa87e28c766eb6ab871e2e1f9
TheGreatestbug <izyspikes@gmail.com>
2023-06-19 23:38:17 +0100

    A program that prints its source file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints its compilation file
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
