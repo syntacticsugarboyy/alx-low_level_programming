801da293acefb3a1c35dcafd8849f773f55bd680
TheGreatestbug <izyspikes@gmail.com>
2023-06-20 01:31:41 +0100

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct dog - dogs
  * @name: Name of dog
@@ -16,5 +19,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
