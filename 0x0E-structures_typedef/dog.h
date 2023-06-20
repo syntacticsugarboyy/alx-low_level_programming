f61d672a74658860a7eaf24b672ed5a16079d89f
TheGreatestbug <izyspikes@gmail.com>
2023-06-20 01:07:21 +0100

    Header file containing struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dogs
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of dog's owner
+ *
+ * Description: A struct containing data of different dogs
+ */
+
 struct dog
 {
 	char *name;
