cd5e3d35c910b1e5e122780662a9dccd4e1f9960
TheGreatestbug <izyspikes@gmail.com>
2023-06-22 04:03:58 +0100

    Selects an operation

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - gets function
+ * @s: pointer to function
+ * @a: First number
+ * @b: Second number
+ *
+ * Description: A function that selects a function
+ *
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	  	{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	{
+		i++;
+	}
+
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Description: A function that adds numbers
+ *
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Description: A function that subtracts numbers
+ *
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Description: A function that multiplies numbers
+ *
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Description: A function that divides numbers
+ *
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulos numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Description: A function that modulos numbers
+ *
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
