a8db12d0e9b4b41eb5e3c15e273a5993cacac83b
TheGreatestbug <izyspikes@gmail.com>
2023-07-04 01:48:45 +0100

    Adds a node.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -29,7 +29,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 	new_node->len = len;
-	ew_node->next = (*head);
+	new_node->next = (*head);
 	(*head) = new_node;
 
 	return (*head);
