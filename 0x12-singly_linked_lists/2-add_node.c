4573ad80c851b7a9b8bccaf8c25895b9b83d8f61
TheGreatestbug <izyspikes@gmail.com>
2023-07-04 01:55:53 +0100

    Adds a node..

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 		len++;
 	}
 
-	new_node = malloc(sizeof(list_t));
+	new_node = (list_t *) malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 	{
@@ -29,6 +29,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 	new_node->len = len;
+
 	new_node->next = (*head);
 	(*head) = new_node;
 
