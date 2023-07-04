3cfa12ed6190b3e8492b54be2b46b6a0aa837ac9
TheGreatestbug <izyspikes@gmail.com>
2023-07-04 01:46:15 +0100

    Adds a node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node
+ * @head: double pointer to head
+ * @str: string
+ *
+ * Description: A function that adds node at the head
+ *
+ * Return: new
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int len;
+	list_t *new_node;
+
+	while (str[len])
+	{
+		len++;
+	}
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = (*head);
+	*head = new_node;
+
+	return (*head);
+}
