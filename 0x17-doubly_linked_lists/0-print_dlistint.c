ffaf98ceabf456e6f9218bdca70a57f8c95903a2
TheGreatestbug <izyspikes@gmail.com>
2023-08-18 22:53:13 +0100

    Prints a soubky linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints a doubly linked list
+ *
+ * Return: num_nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *current;
+	size_t count;
+
+	current = (dlistint_t *)h;
+	count = 0;
+
+	while (current != NULL)
+	{
+		count++;
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+	return (count);
+}
