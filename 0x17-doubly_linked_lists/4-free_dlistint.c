1f8d2cabba17062b3cc82c9f2f7fc56102dcbbbd
TheGreatestbug <izyspikes@gmail.com>
2023-08-30 03:37:50 +0100

    Node manipulation

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - short description
+ * @head: argument_1 description
+ * Return: return description
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	current = head;
+	while (head)
+	{
+		head = head->next;
+		free(current);
+		current = head;
+	}
+}
