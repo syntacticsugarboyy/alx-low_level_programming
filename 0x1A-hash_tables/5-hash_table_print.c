45a8eb5569fc5f9e6849111f5c5ba864c407ba6f
TheGreatestbug <izyspikes@gmail.com>
2023-09-14 01:30:24 +0100

    Prints a hash

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: Pointer to head of hash table
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *p_node;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	printf("{")
+
+	for (index = 0; ht->size != NULL; index++)
+	{
+		p_node = ht->array[index];
+	}
+
+	while (p_node != NULL)
+	{
+		printf("%s: %s", p_node->key, p_node->value);
+
+		if (p_node->next != NULL || index < (ht->size - 1))
+		{
+			printf(", ");
+		}
+		p_node = p_node->next;
+	}
+
+	printf("}\n");
+}
