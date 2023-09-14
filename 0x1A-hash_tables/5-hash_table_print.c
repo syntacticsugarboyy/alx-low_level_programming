f85f58aeb44ae108b4da54e9260eb76bad0a1836
TheGreatestbug <izyspikes@gmail.com>
2023-09-14 01:34:43 +0100

    Prints a hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,17 +22,17 @@ void hash_table_print(const hash_table_t *ht)
 	for (index = 0; ht->size != NULL; index++)
 	{
 		p_node = ht->array[index];
-	}
 
-	while (p_node != NULL)
-	{
-		printf("%s: %s", p_node->key, p_node->value);
-
-		if (p_node->next != NULL || index < (ht->size - 1))
+		while (p_node != NULL)
 		{
-			printf(", ");
+			printf("'%s': '%s'", p_node->key, p_node->value);
+
+			if (p_node->next != NULL || index < (ht->size - 1))
+			{
+				printf(", ");
+			}
+			p_node = p_node->next;
 		}
-		p_node = p_node->next;
 	}
 
 	printf("}\n");
